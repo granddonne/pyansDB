fd56faef6c47788cce94e4de165df3dcc1f9623f
anthony <wangiik@gmail.com>
2021-02-08 19:42:25 +0000

    learning about lists and looping

---
+++ mix-list/inputlistnloop.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+def main():
+    networklists = []
+    with open('driverip.txt', 'r') as driverip:
+        for sline in driverip:
+            networklists.append(sline.rstrip("\n").split(' '))
+
+    print(networklists)
+
+    for driveandip in networklists:
+        print('SSH to ' + driveandip[1] + ' using driver ' + driveandip[0])
+
+main()
---
+++ mix-list/mix_list.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+
+my_list = [ "192.168.0.5", 5060, "UP"]
+
+# below we print all arrays in my_list
+
+print("The first item in the list (IP): " + my_list[0] )
+
+print("The second itme in the list (port): " + str(my_list[1]) )
+
+print("the last item in the list (state): " + my_list[2] )
+
+new_list = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+print("The IP addresses are " + new_list[3] + " and " + new_list[4])
